73f4a89d7631595c8491474487d22167bfafc519
tiberiualex <tiberiu.alexander@gmail.com>
2021-10-09 00:37:00 +0300

    Add registration dispatch

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -2,24 +2,39 @@ import { useForm } from "react-hook-form";
 import styled from "styled-components";
 import { Input, InputContainer } from "./generic/Inputs";
 import { Button } from "./generic/Buttons";
+import { registerUser } from "../state/slices/userSlice";
+import { useAppDispatch, useAppSelector } from "../state/hooks";
+import { User } from "../domain/types";
+
+type FormData = Omit<User, "id">;
 
 const RegistrationForm = () => {
+  const dispatch = useAppDispatch();
+
   const { register, handleSubmit } = useForm();
-  const onSubmit = () => console.log("register");
+  const onSubmit = (data: FormData) => dispatch(registerUser(data));
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <InputContainer>
-        <Input placeholder="Username" type="text" {...register("username")} />
+        <Input
+          placeholder="Username"
+          type="text"
+          {...register("username", { required: true })}
+        />
       </InputContainer>
       <InputContainer>
-        <Input placeholder="Email" type="password" {...register("email")} />
+        <Input
+          placeholder="Email"
+          type="text"
+          {...register("email", { required: true })}
+        />
       </InputContainer>
       <InputContainer>
         <Input
           placeholder="Password"
           type="password"
-          {...register("password")}
+          {...register("password", { required: true })}
         />
       </InputContainer>
       <InputContainer>
