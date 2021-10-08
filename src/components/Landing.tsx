d45eb9dc8333886755dad01dfb0b6ba253c2250d
tiberiualex <tiberiu.alexander@gmail.com>
2021-10-08 03:01:27 +0300

    Add basic styles

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,10 +1,23 @@
 import LoginForm from "./LoginForm";
+import styled from "styled-components";
+
+const LandingContainer = styled.div`
+  background: #fff;
+  border-radius: 8px;
+  padding: 20px;
+
+  box-shadow: 0.1px 0.6px 2.3px rgba(0, 0, 0, 0.026),
+    0.3px 1.3px 5.5px rgba(0, 0, 0, 0.038),
+    0.6px 2.5px 10.4px rgba(0, 0, 0, 0.046),
+    1.1px 4.5px 18.5px rgba(0, 0, 0, 0.054),
+    2.1px 8.4px 34.7px rgba(0, 0, 0, 0.065), 5px 20px 83px rgba(0, 0, 0, 0.09);
+`;
 
 const Landing = () => {
   return (
-    <>
+    <LandingContainer>
       <LoginForm />
-    </>
+    </LandingContainer>
   );
 };
 
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,4 +1,35 @@
 import { useForm } from "react-hook-form";
+import styled from "styled-components";
+
+const Input = styled.input`
+  background-color: #f7f8fa;
+  border-radius: 10px;
+  border: 0;
+  padding: 15px;
+
+  &::placeholder {
+    color: #c9cccf;
+  }
+`;
+
+const InputContainer = styled.div`
+  &:not(:last-child) {
+    margin-bottom: 20px;
+  }
+`;
+
+const Button = styled.button`
+  padding: 15px;
+  background-color: #53b175;
+  border-radius: 10px;
+  border: 0;
+  outline: 0;
+  color: #fff;
+  font-weight: 700;
+  text-align: center;
+  display: inline-block;
+  width: 100%;
+`;
 
 const LoginForm = () => {
   const { register, handleSubmit } = useForm();
@@ -6,9 +37,19 @@ const LoginForm = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" {...register("username")} />
-      <input type="password" {...register("password")} />
-      <button type="submit">Login</button>
+      <InputContainer>
+        <Input placeholder="Username" type="text" {...register("username")} />
+      </InputContainer>
+      <InputContainer>
+        <Input
+          placeholder="Password"
+          type="password"
+          {...register("password")}
+        />
+      </InputContainer>
+      <InputContainer>
+        <Button type="submit">Login</Button>
+      </InputContainer>
     </form>
   );
 };
