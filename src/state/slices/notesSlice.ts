6e2b2827cc8b5c31c2064336efb11fe7be334e5e
tiberiualex <tiberiu.alexander@gmail.com>
2021-10-12 21:26:34 +0300

    Add first notes thunk

--- src/state/slices/notesSlice.ts
+++ src/state/slices/notesSlice.ts
@@ -5,10 +5,30 @@ import {
   EntityAdapter,
   Reducer,
 } from "@reduxjs/toolkit";
+import * as client from "../../api/mockApi";
+import { CreateNoteRequest, Headers, Params } from "../../api/contracts";
+import { Note } from "../../domain/types";
+
+const notesAdapter = createEntityAdapter<Note>();
+const initialState = notesAdapter.getInitialState({
+  loading: false,
+});
+
+export const getUserNotes = createAsyncThunk(
+  "notes/getAll",
+  async (headers: Headers, params: Params, { rejectWithValue }) => {
+    try {
+      const result = client.getUserNotes(headers, params);
+      return result;
+    } catch (err) {
+      rejectWithValue(err);
+    }
+  }
+);
 
 const notesSlice = createSlice({
   name: "notes",
-  initialState: {},
+  initialState,
   reducers: {},
   extraReducers: {},
 });
