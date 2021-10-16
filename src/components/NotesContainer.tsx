25688fb479c567e7b3e4c1864b87f9db45b65ed9
tiberiualex <tiberiu.alexander@gmail.com>
2021-10-17 02:42:56 +0300

    Basic note rendering

--- src/components/NotesContainer.tsx
+++ src/components/NotesContainer.tsx
@@ -1,13 +1,18 @@
 import { useAppDispatch, useAppSelector } from "state/hooks";
 import CreateNoteForm from "./CreateNoteForm";
+import { selectAllNotes } from "state/slices/notesSlice";
+import { Note } from "../domain/types";
 
 const NotesContainer = () => {
-  const notes = useAppSelector((state) => state.notes.entities);
-  console.log(notes);
+  const notes = useAppSelector((state) => selectAllNotes(state));
+
   return (
     <div>
       <h1>Notes</h1>
       <CreateNoteForm />
+      {notes.map((n: Note) => (
+        <p>{n.title}</p>
+      ))}
     </div>
   );
 };
